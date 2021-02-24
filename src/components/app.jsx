49c05a0fa42b84376c2225da8a5627c022402c62
Alice Ratton <aliceratton@gmail.com>
2021-02-23 22:54:10 -0300

    gif and gif list added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line import/extensions
 import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
+    const gifs = [
+      {id: "B1aOlXWCIUYjS"},
+      {id: "iMYbnEuQ36GezAw9nF"}
+    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
-          <div className="selected-gif">
-
-          </div>
+            <div className="selected-gif">
+            <Gif id="iMYbnEuQ36GezAw9nF"/>
+            </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
+      // eslint-disable-next-line indent
       );
   }
 }
