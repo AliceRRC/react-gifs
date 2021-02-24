1a803d36ff0090bf8ba4f83ab135fe23993a5579
Alice Ratton <aliceratton@gmail.com>
2021-02-23 23:33:04 -0300

    done gif and giflists with search bar working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 // eslint-disable-next-line import/extensions
+import giphy from 'giphy-api';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGitId: "iMYbnEuQ36GezAw9nF"
+    }
+    this.search("Homer thinking");
+  }
+
+  search = (query) => {
+    giphy('CmgyM28YKlb3fBngOVotHHRXiPQVkrbF').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+      gifs: result.data
+      });
+    });
+  }
+
   render() {
     const gifs = [
       {id: "B1aOlXWCIUYjS"},
@@ -14,13 +36,13 @@ class App extends Component {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
             <div className="selected-gif">
-            <Gif id="iMYbnEuQ36GezAw9nF"/>
+            <Gif id={this.state.selectedGifId}/>
             </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
       // eslint-disable-next-line indent
